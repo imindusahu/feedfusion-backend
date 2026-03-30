a82be0977e7b42ecba1b2c02b0e4aed5374cb03e
indusahu <indusahu.npc@gmail.com>
2026-03-30 21:37:40 +0530

    add secret key in environment variable

--- app/config.py
+++ app/config.py
@@ -5,3 +5,4 @@ load_dotenv()
 
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
 DATABASE_URL = os.getenv("DATABASE_URL")
+SECRET_KEY = os.getenv("SECRET_KEY")
--- app/utils.py
+++ app/utils.py
@@ -1,10 +1,10 @@
 from passlib.context import CryptContext
 from datetime import datetime, timedelta
 from jose import jwt
+from .config import SECRET_KEY
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-SECRET_KEY = "mysecretkey"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 3
 
