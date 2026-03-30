2e653b47d9ec9261bbe69b2ecffd558ff982dcd3
indusahu <indusahu.npc@gmail.com>
2026-03-30 21:24:57 +0530

    add database url in environment variable

--- app/config.py
+++ app/config.py
@@ -4,3 +4,4 @@ from dotenv import load_dotenv
 load_dotenv()
 
 NEWS_API_KEY = os.getenv("NEWS_API_KEY")
+DATABASE_URL = os.getenv("DATABASE_URL")
--- app/database.py
+++ app/database.py
@@ -1,12 +1,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from .config import DATABASE_URL 
 
-# DATABASE_URL = "sqlite:///./test.db"
-DATABASE_URL = "mysql+pymysql://smart_user:1234@localhost/smart_aggregator"
-
-# engine = create_engine(
-#     DATABASE_URL, connect_args={"check_same_thread": False}
-# )
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
